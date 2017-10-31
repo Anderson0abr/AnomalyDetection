0e050104c2be5740dc2fad1123298152a6b9b22f
Anderson Bezerra <anderson0abr@gmail.com>
2017-10-31 11:55:22 -0700

    Monitor executando em thread (ainda não funciona)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-#!usr/bin/env python
+#!/usr/bin/env python
 # coding: utf-8
 
 """
@@ -11,26 +11,47 @@ Desenvolvido por: Anderson Bezerra Ribeiro
 Data: 30/10/2017
 """
 
-from scapy.all import *
-#import threading
+from Classes.myThread import MyThread
+from scapy.all import rdpcap, conf
+from time import sleep
+
+import subprocess
+
+def monitor(name):
+  print(name + " running...")
+  subprocess.run(["sudo", "tcpdump", "ip", "-Uni", "ens33", "-w", "./capture.pcap"])
+  #pkts = sniff(iface=conf.iface, count=50, store=1, filter="ip")
 
 if __name__ == "__main__":
- ipHeader = []
- ipTable = []
+  ipHeader = []
+  ipTable = []
 
- pkts = sniff(iface=conf.iface, count=50, store=1, filter="ip")
+  thread_monitor = MyThread(monitor, ("Monitor",))
+  thread_monitor.start()
 
- for pkt in pkts:
-  ipPkt = pkt.payload
-  if ipPkt.src not in ipHeader:
-   ipHeader.append(ipPkt.src)
-   ipTable.append([])
-   ipTable[-1].append(pkt)
-  else:
-   ipTable[ipHeader.index(ipPkt.src)].append(pkt)
+  while True:
+    try:
+      pkts = rdpcap("./capture.pcap")
 
-for i in range(len(ipHeader)):
-	print(ipHeader[i])
-	for j in range(len(ipTable[i])):
-		print(ipTable[i][j].summary())
-	print(" ")
+      if pkts:
+        for pkt in pkts:
+          ipPkt = pkt.payload
+          if ipPkt.src not in ipHeader:
+            ipHeader.append(ipPkt.src)
+            ipTable.append([])
+            ipTable[-1].append(pkt)
+          else:
+            ipTable[ipHeader.index(ipPkt.src)].append(pkt)
+
+        for i in range(len(ipHeader)):
+          print(ipHeader[i])
+          for j in range(len(ipTable[i])):
+            print(ipTable[i][j].summary())
+          print(" ")
+          
+    except IOError as err:
+      print(err)
+      sleep(5)
+      continue
+
+    
