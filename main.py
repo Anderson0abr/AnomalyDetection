9d2c40a5372765e1283be18c823e45e6ef189e31
Anderson Bezerra <anderson0abr@gmail.com>
2017-11-14 11:32:24 -0800

    Classificação dos pacotes agora utiliza Ips e portas de origem e destino e protocolo da segunda camada

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ Data: 30/10/2017
 from Classes.myThread import MyThread
 from scapy.all import *
 from time import time, sleep
-from random import randrange
+from random import randrange, choice
 
 import threading
 
@@ -30,7 +30,7 @@ def test():
     sleep(10)
     for i in range(4):
       send(IP(src=srcs[1], dst=dest)/TCP(), verbose=0)
-      send(IP(src=srcs[2], dst=dest)/TCP(), verbose=0)
+      send(IP(src=srcs[2], dst=dest)/UDP(), verbose=0)
       send(IP(src=srcs[3], dst=dest)/TCP(), verbose=0)
     send(IP(src=srcs[0], dst=dest)/TCP(), verbose=0)
     sleep(10)
@@ -40,12 +40,12 @@ def stressTest():
   print("Starting stress test...")
   dest="10.10.10.10"
   while True:
-    send(IP(src="192.30.253."+str(randrange(100)), dst=dest)/TCP(), verbose=0)
+    send(IP(src="192.30.253."+str(randrange(100)), dst=dest)/choice([TCP(),UDP()]), verbose=0)
     sleep(1)
 
 def printToFile(file):
   for i in range(len(ipHeader)):
-    file.write(ipHeader[i] + "\n")
+    file.write("IpSource: " + str(ipHeader[i][0]) + " IpDest: " + str(ipHeader[i][1]) + " L2Protocol: " + str(ipHeader[i][2]) + " SourcePort: " + str(ipHeader[i][3]) + " DestPort: " + str(ipHeader[i][4]) + "\n")
     for j in range(len(ipTable[i])):
       file.write(ipTable[i][j].summary() + "\n")
     file.write("\n")
@@ -57,9 +57,10 @@ def timer():
     rewrite = False
     tableMutex.acquire()
     for i in reversed(range(len(ipHeader))): # Começa a checar do último para evitar erro de indexação
+      headerPkt = ipHeader[i]
       lastReference = time() - ipTimer[i]
       if lastReference > tempoLimite:
-        print("- Removed package... IP:", ipHeader[i], "Last reference:", lastReference, "seconds ago")
+        print("- Removed package... IpSource:", headerPkt[0], "IpDest:", headerPkt[1], "L2Protocol:", headerPkt[2], "SourcePort:", headerPkt[3], "DestPort:", headerPkt[4])
         ipTimer.pop(i)
         ipHeader.pop(i)
         ipTable.pop(i)
@@ -71,24 +72,31 @@ def timer():
 
 def monitorCallback(pkt):
   ipPkt = pkt.payload
-  print("+ Package captured... IP:", ipPkt.src)
+  l2Pkt = ipPkt.payload
+  if ipPkt.proto == 6:
+    l2Protocol = "tcp"
+  elif ipPkt.proto == 17:
+    l2Protocol = "udp"
+
+  headerPkt = [ipPkt.src, ipPkt.dst, l2Protocol, l2Pkt.sport, l2Pkt.dport]
+  print("+ Package captured... IpSource:", headerPkt[0], "IpDest:", headerPkt[1], "L2Protocol:", headerPkt[2], "SourcePort:", headerPkt[3], "DestPort:", headerPkt[4])
   tableMutex.acquire()
-  if ipPkt.src not in ipHeader:
-    ipHeader.append(ipPkt.src)
+  if headerPkt not in ipHeader:
+    ipHeader.append(headerPkt)
     ipTimer.append(time())
     ipTable.append([])
     ipTable[-1].append(pkt)
   else:
-    ipTimer[ipHeader.index(ipPkt.src)] = time()
-    ipTable[ipHeader.index(ipPkt.src)].append(pkt)
+    ipTimer[ipHeader.index(headerPkt)] = time()
+    ipTable[ipHeader.index(headerPkt)].append(pkt)
   with open("IP_Log.txt", "w") as file:
     printToFile(file)
   tableMutex.release()
 
 if __name__ == "__main__":
   initialTime = time()
-  checkTime = 10
-  tempoLimite = 5*60 # 5 minutos
+  checkTime = 30 # 30 segundos
+  tempoLimite = 15*60 # 15 minutos
   tableMutex = threading.Semaphore(1)
 
   ipHeader = []
@@ -100,7 +108,7 @@ if __name__ == "__main__":
   thread_timer.start()
   test_thread.start()
 
-  sniff(iface="root-eth0", filter="ip and tcp", prn=monitorCallback)
+  sniff(iface="root-eth0", filter="ip", prn=monitorCallback)
   #root --> 10.10.10.254
   #eth0 --> 10.10.10.10
 
