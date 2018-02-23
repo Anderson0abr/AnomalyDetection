9be12abfc5e3b4df6208f8360fae5171cab1d578
Anderson Bezerra <anderson0abr@gmail.com>
2018-02-23 12:13:16 -0800

    Remoção da coluna Referencias e criação do Notebook

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from time import time, sleep
 from random import randrange, choice
 
 import threading
-import pandas
+import pandas as pd
 
 def test():
   #checktime 5s limite 20s
@@ -43,10 +43,9 @@ def stressTest():
   dest="10.10.10.10"
   while True:
     send(IP(src="192.30.253."+str(randrange(100)), dst=dest)/choice([TCP(),UDP()]), verbose=0)
-    sleep(1)
 
 def listToDF(row):
-  return pandas.DataFrame(data=[row], columns=columns)
+  return pd.DataFrame(data=[row], columns=columns)
 
 def isRowInDF(row):
   global ipDf
@@ -59,17 +58,16 @@ def appendRowInDF(row):
 def updateRowInDF(row):
   global ipDf
   index = ipDf[(ipDf[columns[0]] == row[0]) & (ipDf[columns[1]] == row[1]) & (ipDf[columns[2]] == row[2]) & (ipDf[columns[3]] == row[3]) & (ipDf[columns[4]] == row[4])].index
-  ipDf.loc[index, columns[6]] += 1
-  ipDf.loc[index, columns[7]] = pandas.Timestamp('now')
+  ipDf.loc[index, columns[6]] = pd.Timestamp('now')
 
 def deleteExpiredRowsInDF():
   global ipDf
-  df = ipDf[(pandas.Timestamp('now') - ipDf["Last reference"]) <= tempoLimite]
+  df = ipDf[(pd.Timestamp('now') - ipDf["Last reference"]) <= tempoLimite]
   if df.equals(ipDf):
     return False
   else:
-    for index, row in ipDf[(pandas.Timestamp('now') - ipDf["Last reference"]) > tempoLimite].iterrows():
-      print("- Removed package... Index:", index, "IpSource:", row[columns[0]], "IpDest:", row[columns[1]], "L2Protocol:", row[columns[2]], "SourcePort:", row[columns[3]], "DestPort:", row[columns[4]], "Package size:", row[columns[5]], "References:", row[columns[6]])
+    for index, row in ipDf[(pd.Timestamp('now') - ipDf["Last reference"]) > tempoLimite].iterrows():
+      print("- Removed package... Index:", index, "IpSource:", row[columns[0]], "IpDest:", row[columns[1]], "L2Protocol:", row[columns[2]], "SourcePort:", row[columns[3]], "DestPort:", row[columns[4]], "Package size:", row[columns[5]])
     ipDf = df
     return True
 
@@ -81,7 +79,7 @@ def timer():
     tableMutex.acquire()
     deleted = deleteExpiredRowsInDF()
     if deleted:
-      ipDf.to_csv(file)
+      ipDf[["IP source", "IP destiny", "L2 protocol", "Source port", "Destiny port", "Package size"]].to_csv(file)
     tableMutex.release()
 
 def monitorCallback(pkt):
@@ -95,37 +93,36 @@ def monitorCallback(pkt):
   elif ipPkt.proto == 17:
     l2Protocol = "udp"
 
-  rowPkt = [ipPkt.src, ipPkt.dst, l2Protocol, l2Pkt.sport, l2Pkt.dport, len(pkt), 1, pandas.to_datetime("now")]
+  rowPkt = [ipPkt.src, ipPkt.dst, l2Protocol, l2Pkt.sport, l2Pkt.dport, len(pkt), pd.to_datetime("now")]
   print("+ Package captured... IpSource:", rowPkt[0], "IpDest:", rowPkt[1], "L2Protocol:", rowPkt[2], "SourcePort:", rowPkt[3], "DestPort:", rowPkt[4])
   tableMutex.acquire()
   if isRowInDF(rowPkt):
     updateRowInDF(rowPkt)
   else:
     appendRowInDF(rowPkt)
-  ipDf.to_csv(file)
+  ipDf[["IP source", "IP destiny", "L2 protocol", "Source port", "Destiny port", "Package size"]].to_csv(file)
   tableMutex.release()
 
 if __name__ == "__main__":
-  file = "IP_DataFrame.csv"
-  checkTime = 5 # 30 segundos
-  tempoLimite = pandas.Timedelta('20s') # 15 minutos
+  file = "IP_DataFrame2.csv"
+  checkTime = 30 # 30 segundos
+  tempoLimite = pd.Timedelta('15m') # 15 minutos
   tableMutex = threading.Semaphore(1)
 
   #Criando DataFrame e definindo tipos
-  columns = ["IP source", "IP destiny", "L2 protocol", "Source port", "Destiny port", "Package size", "References", "Last reference"]
-  ipDf = pandas.DataFrame(columns = columns)
-  ipDf["L2 protocol"] = ipDf["L2 protocol"].astype("category")
-  ipDf[columns[3:7]] = ipDf[columns[3:7]].astype("int")
-  ipDf["Last reference"] = pandas.to_datetime(ipDf["Last reference"])
+  columns = ["IP source", "IP destiny", "L2 protocol", "Source port", "Destiny port", "Package size", "Last reference"]
+  ipDf = pd.DataFrame(columns = columns)
+  ipDf[columns[3:6]] = ipDf[columns[3:6]].astype("int")
+  ipDf["Last reference"] = pd.to_datetime(ipDf["Last reference"])
 
   thread_timer = MyThread(timer, ())
-  test_thread = MyThread(test, ())
+  test_thread = MyThread(stressTest, ())
   thread_timer.start()
   test_thread.start()
 
-  sniff(iface="root-eth0", filter="ip", prn=monitorCallback, count=20)
+  sniff(iface="root-eth0", filter="ip", prn=monitorCallback, count=1000)
   #root --> 10.10.10.254
   #eth0 --> 10.10.10.10
 
-  for index, row in ipDf.iterrows():
-    print("Index:", index, "IpSource:", row[columns[0]], "IpDest:", row[columns[1]], "L2Protocol:", row[columns[2]], "SourcePort:", row[columns[3]], "DestPort:", row[columns[4]], "Package size:", row[columns[5]], "References:", row[columns[6]])
+  #for index, row in ipDf.iterrows():
+  #  print("Index:", index, "IpSource:", row[columns[0]], "IpDest:", row[columns[1]], "L2Protocol:", row[columns[2]], "SourcePort:", row[columns[3]], "DestPort:", row[columns[4]], "Package size:", row[columns[5]])
